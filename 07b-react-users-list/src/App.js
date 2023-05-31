e608967df4b0765e9f4f0bd8454426db9888eede
nicolesayuri <nicole.sayuri2@gmail.com>
2023-05-30 21:07:41 -0300

    30-05 - aula

--- 07b-react-users-list/src/App.js
+++ 07b-react-users-list/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
 function App() {
+  const users = [
+    {
+      id: 0,
+      name: 'Siri'
+    },
+    {
+      id: 1,
+      name: 'Alexa'
+    },
+    {
+      id: 2,
+      name: 'Google Assistente'
+    }
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List items={users} />
     </div>
   );
 }
